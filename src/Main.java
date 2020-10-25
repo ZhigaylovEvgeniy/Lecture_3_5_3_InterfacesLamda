2e5691708931f0ad3c732279beaafe6a6cdfacc0
zhigan <zhigan639@gmail.com>
2020-10-25 17:16:49 +0500

    Version 3.0

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
         Timer timer1 = new Timer();
-        long time = timer1.measureTime(new Runnable(){
-            @Override
-            public void run(){
-                new BigDecimal("1234567").pow(100000);
-            }
+        long time = timer1.measureTime(() -> {
+            new BigDecimal("1234567").pow(100000);
         });
         System.out.println(time);
     }
